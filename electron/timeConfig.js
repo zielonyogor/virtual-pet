9b4aef23b232c04103ceecd1c4e42ddd57216069
Katarzyna Jaromirska <kasia.jaromirska@gmail.com>
2025-07-18 15:39:02 +0200

    add some time styling

--- electron/timeConfig.js
+++ electron/timeConfig.js
@@ -3,6 +3,22 @@ const path = require('path');
 
 const timesJSONPath = path.join(process.cwd(), 'data', 'times.json');
 
+function getTimes() {
+    if(!fs.existsSync(timesJSONPath)) return [];
+        
+    try {
+        return JSON.parse(fs.readFileSync(timesJSONPath, 'utf8'));
+    } catch (error) {
+        console.error(`Failed to load JSON with times: ${error}`);
+        return [];   
+    }  
+}
+
+function setTimes(newTimes) {
+
+}
+
 module.exports = {
-    
+    getTimes,
+    setTimes,
 }
